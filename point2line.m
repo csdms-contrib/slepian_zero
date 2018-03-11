872c22ec65cf8e12681addcade4d28bab374ae22
Frederik J. Simons <fjsimons@gmail.com>
2018-03-11 11:47:27 -0400

    Additional functionality and bug fixes

--- point2line.m
+++ point2line.m
@@ -1,5 +1,5 @@
 function d=point2line(x1,x2,y1,y2,x0,y0)
-% d=POINT2LINE(x1,x2,y1,y2,x0,y0))
+% d=POINT2LINE(x1,x2,y1,y2,x0,y0)
 %
 % Calculates the perpendicular distance of a single point (x0,y0) onto the
 % line with the pair of coordinates (x1,y1) and (x2,y2)
@@ -12,6 +12,6 @@ function d=point2line(x1,x2,y1,y2,x0,y0)
 % SEE ALSO:
 % http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
 %
-% Last modified by fjsimons-at-alum.mit.edu, 11/30/2015
+% Last modified by fjsimons-at-alum.mit.edu, 10/17/2017
 
 d=abs([x2-x1]*[y1-y0]-[x1-x0]*[y2-y1])/sqrt([x2-x1]^2+[y2-y1]^2);
