5649e1a28016c24e28f79a4230cf285ae3040c58
Frederik J. Simons <fjsimons@gmail.com>
2018-03-12 23:04:00 -0400

    Distance of a SINGLE POINT to a line between a PAIR of points in the plane

--- point2line.m
+++ point2line.m
@@ -2,7 +2,7 @@ function d=point2line(x1,x2,y1,y2,x0,y0)
 % d=POINT2LINE(x1,x2,y1,y2,x0,y0)
 %
 % Calculates the perpendicular distance of a SINGLE point (x0,y0) onto the
-% line with the PAIR of coordinates (x1,y1) and (x2,y2)
+% line with the PAIR of coordinates (x1,y1) and (x2,y2) in the plane.
 %
 % NOTE:
 % 
@@ -12,6 +12,6 @@ function d=point2line(x1,x2,y1,y2,x0,y0)
 % SEE ALSO:
 % http://mathworld.wolfram.com/Point-LineDistance2-Dimensional.html
 %
-% Last modified by fjsimons-at-alum.mit.edu, 3/11/2018
+% Last modified by fjsimons-at-alum.mit.edu, 3/12/2018
 
 d=abs([x2-x1]*[y1-y0]-[x1-x0]*[y2-y1])/sqrt([x2-x1]^2+[y2-y1]^2);
