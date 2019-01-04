677ca6018cb0556b9385c50953c608016dfca5be
Frederik J. Simons <fjsimons@gmail.com>
2019-01-03 22:33:40 -0500

    Calculates means of submatrices within a matrix

--- blockmean.m
+++ blockmean.m
@@ -1,23 +1,18 @@
-function [bm,xc,yc,CT]=blockmean(mat,side,olap)
-% [bm,xc,yc,CT]=BLOCKMEAN(mat,[iside jside],[olapi olapj])
+function [bm,xc,yc]=blockmean(mat,side,olap)
+% [bm,xc,yc]=BLOCKMEAN(mat,[iside jside],[olapi olapj])
 %
 % Block-averages a matrix, with or without overlap in the block tiles
-% When working with real coordinates, of course these indices
-% are pixel-centered. These then are the centers for the averaging
-% regions; with the overlap, it is not possible to use them as the pixel
-% centers for an image plot. 
 %
 % INPUT:
 %
 % mat         A certain matrix
 % side        [iside jside] block size
-% olap        [olapi olapj] overlap size
+% olap        [olapi olapj] overlap size (default: 0 for no overlap))
 %
 % OUTPUT:
 %
 % bm          The matrix of means as requested
 % xc,yc       The center point of the boxes
-% CT          The sparse matrix at the heart of it all
 %
 % TEST EXAMPLES THAT SHOULD PRODUCE NO OUTPUT:
 %
@@ -32,6 +27,13 @@ function [bm,xc,yc,CT]=blockmean(mat,side,olap)
 %   diferm(mean(tile(:))-indeks(blockmean(mat,[20 20],[10 10]),index))
 % end
 %
+% NOTE:
+%
+% When working with real coordinates, of course these indices
+% are pixel-centered. These then are the centers for the averaging
+% regions; with the overlap, it is not possible to use them as the pixel
+% centers for an image plot. 
+%
 % SEE ALSO:
 % 
 % GAMINI, PAULI, PCHAVE, BLOCKMEAN, ...
@@ -41,52 +43,62 @@ function [bm,xc,yc,CT]=blockmean(mat,side,olap)
 % Parse the second input
 [iside,jside]=deal(side(1),side(2));
 
-% Could get rid of first if-statement and just make default overlap zero.
+% Just make default overlap zero
+defval('olap',[0 0])
 
-% Non-overlapping tiles
-if nargin==2
+% Overlapping tiles, parse the third input or whatever the default was
+[olapi,olapj]=deal(olap(1),olap(2));
+
+% Non-overlapping tiles, precisely fitting
+if olapi==0 && olapj==0
   if any(mod(size(mat),side))
     error('Matrix not right size for nonoverlapping tiles')
   end
-  % Column space averaging
+  % Prepare the column space averaging
   ro=1:size(mat,2);
-  co=gamini(1:length(ro)/jside,jside);
-  CT=sparse(ro,co,1);
-  post=mat*CT;
-  % Row space averaging
-  ro=1:size(mat,1);
-  co=gamini(1:length(ro)/iside,iside);
-  CT=sparse(ro,co,1)';
-  bm=CT*post;
-  bm=bm/prod(side);
-  [xc,yc]=deal(NaN);
 else
-  % Overlapping tiles, parse the third input
-  [olapi,olapj]=deal(olap(1),olap(2));
+  % Overlapping tiles, precisely fitting
   [ny,nx]=size(mat);
-  % Number of windows in X
+  % Number of tiles in X
   nwj=(nx-olapj)/(jside-olapj); 
-  % Number of windows in Y
+  % Number of tiles in Y
   nwi=(ny-olapi)/(iside-olapi); 
   if ~all(fix([nwi nwj])==[nwi nwj])
     error('Matrix not right size for overlapping tiles')
   end  
-  % Column space averaging
+  % Prepare the column space averaging
   ro=pauli(1:size(mat,2),jside);
   ro=ro(1:jside-olapj:end,:)';
   ro=ro(:)';
-  co=gamini(1:length(ro)/jside,jside);
-  CT=sparse(ro,co,1);
-  post=mat*CT;
-  % Row space averaging
+end
+
+% Perform the column-space averaging
+co=gamini(1:length(ro)/jside,jside);
+CT=sparse(ro,co,1);
+post=mat*CT;
+
+% Non-overlapping tiles, precisely fitting
+if olapi==0 && olapj==0
+  % Prepare row space averaging
+  ro=1:size(mat,1);
+else
+  % Overlapping tiles, precisely fitting
+  % Prepare row space averaging
   ro=pauli(1:size(mat,1),iside);
   ro=ro(1:iside-olapi:end,:)';
   ro=ro(:)';
-  co=gamini(1:length(ro)/iside,iside);
-  CT=sparse(ro,co,1)';
-  bm=CT*post;
-  bm=bm/prod(side);
-  xc=(1+(jside-1)/2):jside-olapj:size(mat,2);
-  yc=(1+(iside-1)/2):iside-olapi:size(mat,1);
 end
+
+% Perform the row-space averaging
+co=gamini(1:length(ro)/iside,iside);
+CT=sparse(ro,co,1)';
+bm=CT*post;
+
+% Normalization at the very end
+bm=bm/prod(side);
+
+% Output that will only make sense for the overlap
+xc=(1+(jside-1)/2):jside-olapj:size(mat,2);
+yc=(1+(iside-1)/2):iside-olapi:size(mat,1);
+
 
