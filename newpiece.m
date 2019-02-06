2a78b2d59a9fc3f528608af7a331f59832a925be
Frederik J. Simons <fjsimons@gmail.com>
2019-02-06 18:11:44 -0500

    My greatest code, fixing a mistake

--- newpiece.m
+++ newpiece.m
@@ -1,6 +1,6 @@
 % Hello
-% Last modified by fjsimons-at-alum.mit.edu, 08/05/1999
+% Last modified by fjsimons-at-alum.mit.edu, 08/05/8888
 
 read file
 use numbers
-output= 2*numbers
+output= 3*numbers
