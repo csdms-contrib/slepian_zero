1d4264871b3940d328ed4f856922648f640fce9f
Frederik J. Simons <fjsimons@gmail.com>
2019-09-09 00:10:38 -0400

    Checks overlapping rims in a tile set

--- rimcheck.m
+++ rimcheck.m
@@ -8,17 +8,18 @@ function [A,B]=rimcheck(A,B,rim,M,xver)
 % A,B      Two matrices, not necessarily the same size, some overlap
 % rim      The rim size of possible overlap
 % M        The PUZZLE match code
-% xver     1 issue warnings
-%          0 do not issue any warnings 
+% xver     1 issue warnings relevant for particular alignment situations
+%            only in some of the cases relevant to TINITALY
+%          0 do not issue any warnings [default]
 %
 % OUTPUT:
 %
 % A,B      The two matrices where one was trimmed to avoid the overlap
 %
-% Last modified by fjsimons-at-alum.mit.edu, 05/24/2019
+% Last modified by fjsimons-at-alum.mit.edu, 09/08/2019
 
 % Default is to issue warnings (but no confirmation messages!)
-defval('xver',1)
+defval('xver',0)
 
 % In TINITALH, higher numbers are typically to the right or up... but I
 % have preferred to spell out the cases where the individual lines are
@@ -27,8 +28,9 @@ switch M
  case 8
   % [D1   D2] is the match with overlap
   % Just use the case below with switched inputs
-  [B,A]=rimcheck(B,A,rim,4);
-  [A,B]=deal(B,A);
+  [B,A]=rimcheck(B,A,rim,4,xver);
+  % The next line was in error but didn't matter
+  %[A,B]=deal(B,A);
  case 4
   % [D2   D1] is the match with overlap
   if size(B,1)<size(A,1)
@@ -37,7 +39,7 @@ switch M
     % Now B gets trimmed on the right
     B=B(:,1:end-rim,:);
   else
-    % B has a larger COLUMN size and is to the LEFT of A
+    % B has a larger ROW size and is to the LEFT of A
     if xver==1; diferm(B(1:size(A,1),end-rim+1:end)-A(:,1:rim)); end
     % Now A gets trimmed from the left
     A=A(:,rim+1:end);
@@ -45,8 +47,9 @@ switch M
  case 2
   % [D1 ; D2] is the match with overlap
   % Just use the case below with switched inputs
-  [B,A]=rimcheck(B,A,rim,1);
-  [A,B]=deal(B,A);
+  [B,A]=rimcheck(B,A,rim,1,xver);
+  % The next line was in error but didn't matter
+  %[A,B]=deal(B,A);
  case 1
   % [D2 ; D1] is the match with overlap
   if size(B,2)<size(A,2)
