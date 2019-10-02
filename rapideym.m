cb6093cfef7ae14c1b31e25f34c8ebaec5883992
Frederik J. Simons <fjsimons@gmail.com>
2019-10-02 01:35:45 -0400

    Combines multiple RAPIDEYE images into a single MAT file

--- rapideym.m
+++ rapideym.m
@@ -14,7 +14,7 @@ function rapideym(diro)
 % YYYYMMDD_HHMMSS_GGGGGGG_RapidEye-N where GGGGGGGG is grid cell and N
 % the satellite id and YYYYMMDD_HHMMSS the date acquired
 %
-% Last modified by Last modified by fjsimons-at-alum.mit.edu, 09/30/2019
+% Last modified by Last modified by fjsimons-at-alum.mit.edu, 10/02/2019
 
 % Default
 defval('diro',fullfile(getenv('ITALY'),'RAPIDEYE','enotre'))
@@ -41,6 +41,8 @@ end
 
 % See below at 'orchard' for old syntax instead of the subfunction
 spitout(sname,'tox',tox); clear tox
+
+% AT THIS POINT YOU ARE STARTING A NEW FILE!! 
 % Save the variable named sname into the file named fname to initialize
 savit(sname)
 
@@ -60,6 +62,65 @@ for index=1:length(dirp)
   froot=froot{1}(1:strfind(froot{1},'_Analytic_')-1);
   % Now read the image, don't bother with setting xver=2 after a while
   [alldata,nprops,props,rgbdata,alfadat]=rapideye(froot,dirp{index},diro,1,[],clip);
+
+  % BEGIN REMOVE RIMMING
+
+  % Find the grid
+  [XE,YE]=rapideyg(nprops);
+
+  % This is where you trim the data that are ALL zeros
+  % How about we remove rinds from here - zero rows/columns, that'll do it
+  emptycols=sum(sum(alldata,3)==0,1)==size(alldata,1);
+  emptyrows=sum(sum(alldata,3)==0,2)==size(alldata,2);
+
+  % Assuming that they are AT the edges (no interior removal; not sure if
+  % when THAT would be caught down the line)
+  if sum(emptycols) || sum(emptyrows)
+    disp(sprintf('Empty-border trimming'))
+    XE=XE(~emptyrows,~emptycols);
+    YE=YE(~emptyrows,~emptycols);
+    alldata=alldata(~emptyrows,~emptycols,:);
+  end
+
+  % This is where you trim the edges if they contain ANY zeros; 
+  % The mask as we define it HAS data
+  mask=sum(alldata,3)~=0;
+  % BEGIN IMAGE TRIMMING
+  if prod(size(mask))~=size(alldata(:,:,1))
+    disp(sprintf('Any-border  trimming'))
+    % Collect coordinates
+    mark=reshape(1:prod(size(mask)),size(mask));
+    % Now we find the (largest?) rectangle that fits inside the mask
+    % MASK2RECT; go around the corner, see if it works
+    mark=mark(:,  min(find(mask(1,:  ))):end);
+    mask=mask(:,  min(find(mask(1,:  ))):end);
+    mark=mark(    min(find(mask(:,end))):end,:);
+    mask=mask(    min(find(mask(:,end))):end,:);
+    mark=mark(  1:max(find(mask(:,1  ))),:);
+    mask=mask(  1:max(find(mask(:,1  ))),:);
+    mark=mark(:,1:max(find(mask(end,:))));
+    mask=mask(:,1:max(find(mask(end,:))));
+    % Maybe do this again if it still has NaNs? Maybe rotate the image??
+    % All permutations? This works for enotre, but need to inspect
+    trimdata=zeros(size(mask),class(alldata));
+    for ondex=1:size(alldata,3)
+      data=alldata(:,:,ondex);
+      trimdata(:,:,ondex)=reshape(data(mark),size(mask));
+    end
+    XE=reshape(XE(mark),size(mask));
+    YE=reshape(YE(mark),size(mask));
+    alldata=trimdata;
+    % Now adjust the metadata like in MOSAIC of course
+    nprops.nr=size(alldata,1);
+    nprops.nc=size(alldata,2);
+    nprops.xs=XE(1)-nprops.sp/2;
+    nprops.ys=YE(1)+nprops.sp/2;
+    nprops.C11=[XE(1)   YE(1)];
+    nprops.CMN=[XE(end) YE(end)];
+    nprops.xx=XE([1 end]); 
+    nprops.yy=YE([1 end]);
+  end
+  % END IMAGE TRIMMING
   
   % The precision of these variables remains what they were
   spitout(vname,'alldata',alldata)
@@ -70,12 +131,15 @@ for index=1:length(dirp)
   savito(vname)
 end
 
+% Should I save the LARGEST EVER nprops for the TOPODATA
+
 % Add table of contents
 spitout(sname,'tox',tox)
 
 % Now add the topography and rivers for the same region
 % Again don't bother with setting xver=2 after a while
 [TDF,~,SX,SY]=tinitaly(nprops,[],[],1,alldata);
+
 % Get the whole grid out, always verify equal spacing
 [~,~,~,xeye,yeye]=rapideyg(nprops,1);
 % This is going to be a ROW vector
@@ -91,14 +155,17 @@ spitout(sname,'C11',C11)
 CMN=nprops.CMN; 
 spitout(sname,'CMN',CMN)
 % The following is more intuitive with IMAGESC
-xx=xeye([1 end]);
-yy=yeye([1 end]);
+xx=xeye([1 end]); xx=xx(:)';
+yy=yeye([1 end]); yy=yy(:)';
 spitout(sname,'xx',xx)
 spitout(sname,'yy',yy)
 spitout(sname,'xeye',xeye)
 spitout(sname,'yeye',yeye)
 % Make a snug axis
+% I first used this
 snug=[nprops.xp(1) nprops.xp(2) nprops.yp(3) nprops.yp(1)];
+% But those are properties I didn't adjust after trimming, so...
+
 spitout(sname,'snug',snug)
 spitout(sname,'riverx',SX)
 spitout(sname,'rivery',SY)
