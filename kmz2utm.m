9d6e2e6bc9167fbf24b982c544b3a2752eb87774
Frederik J. Simons <fjsimons@gmail.com>
2019-10-09 10:04:16 -0400

    Conversion of KMZ file to UTM coordinates

--- kmz2utm.m
+++ kmz2utm.m
@@ -7,7 +7,7 @@ function varargout=kmz2utm(fname)
 %
 % INPUT:
 %
-% fname          A KMZ filename with (so far, a single) trace
+% fname          A filename with (so far, a single) trace. No extension
 %
 % OUTPUT:
 %
@@ -22,14 +22,14 @@ function varargout=kmz2utm(fname)
 % figure(1); clf
 % [alldata,nprops]=rapideye('3357121_2019-03-04_RE1_3A','20190304_094134_3357121_RapidEye-1','enotre',[],[],'_clip');
 % imagesc([nprops.C11(1) nprops.CMN(1)],[nprops.C11(2) nprops.CMN(2)],rapideya(alldata)); hold on
-% [xe,ye,ze]=kmz2utm('enotre/SouthEnotreUrbanMesoraca.mat'); p(1)=plot(xe,ye,'y','LineWidth',1); hold off
+% [xe,ye,ze]=kmz2utm('enotre/SouthEnotreUrbanMesoraca'); p(1)=plot(xe,ye,'y','LineWidth',1); hold off
 %
 %% FROM A PRESAVED SATELLITE IMAGE STACK:
 % 
 % figure(2); clf
 % load('enotre/ri_enotre.mat','enotre_20190304094134')
 % imagesc(enotre_20190304094134.nprops.xx,enotre_20190304094134.nprops.yy,rapideya(enotre_20190304094134.alldata)); hold on
-% [xe,ye,ze]=kmz2utm('enotre/SouthEnotreUrbanMesoraca.mat'); p(1)=plot(xe,ye,'y','LineWidth',1); hold off
+% [xe,ye,ze]=kmz2utm('enotre/SouthEnotreUrbanMesoraca'); p(1)=plot(xe,ye,'y','LineWidth',1); hold off
 %
 % Last modified by fjsimons-at-alum.mit.edu, 10/09/2019
 
@@ -37,7 +37,7 @@ function varargout=kmz2utm(fname)
 if exist(sprintf('%s.mat',pref(fname)))~=2
   if exist(sprintf('%s.txt',pref(fname)))~=2
      % Transform the file in question with UNZIP and KML2GMT (which you need!)
-     system(sprintf('unzip %s',fname));
+     system(sprintf('unzip %s.kmz',fname));
      % This to get rid of possible future timestamps
      system(sprintf('touch %s','doc.kml'));
      system(sprintf('kml2gmt  %s | awk ''NR>3 {print}'' >! %s.txt','doc.kml',pref(fname)));
