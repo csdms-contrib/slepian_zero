ceeacae517b4a4f4ddf4b600c412d99452e24a81
Frederik J. Simons <fjsimons@gmail.com>
2020-07-06 17:47:33 -0400

    Creates sample pairs with a prescribed covariance structure

--- fakerelation.m
+++ fakerelation.m
@@ -1,11 +1,23 @@
-function [X,Y,CXY,Z1,Z2]=fakerelation(C12,N,distro,pars)
+function [X,Y,CXY,C12,Z1,Z2]=fakerelation(C12,N,distro,ploti)
+% [X,Y,CXY,C12,Z1,Z2]=fakerelation(C12,N,distro,ploti)
+%
 % Creates data pairs with a particular relation
 %
 % INPUT:
 %
+% C12       The three ELEMENTS of the desired population covariance matrix
+%           [CXX CXY CYY] <---- watch this order!
+% N         The sample size [default: 100]
+% distro    The population distribution [default: 'norm']
+% ploti     1 Make a little plot
+%           0 Don't [default]
 %
 % OUTPUT:
 %
+% X, Y      The data sample
+% CXY       The sample covariance
+% C12       The population covariance MATRIX
+% Z1, Z2    The uncorrelated generating variables
 %
 % Last modified by fjsimons-at-alum.mit.edu, 02/12/2020
 
@@ -13,10 +25,13 @@ function [X,Y,CXY,Z1,Z2]=fakerelation(C12,N,distro,pars)
 defval('N',25);
 
 % Define the population covariance matrix
-defval('C12',[1 0.5 ; 0.5 1])
+defval('C12',[1 0.5 1])
+% Rearrange to make sure
+C12=[C12(1) C12(2) ; C12(2) C12(3)];
 
 % Define a class of distributions
 defval('distro',{'norm'});
+% You don't really want to change this, ever
 defval('pars',  {[0 1]})
 
 % Calculate the initial uncorrelated random variables
@@ -36,11 +51,13 @@ Y=XY(:,2);
 % Report on their sample covariance
 CXY=cov(X,Y);
 
-% Plots and Cosmetics
-% plot(X,Y,'o')
-% set(gca,'ytick',1:ld,'yticklabel',letters(1:ld,1))
-% set(gca,'ydir','rev')
-% fig2print(gcf,'portrait')
-% ylim(xpand(ylim))
-% longticks(gca,2)
-% figdisp([],[],[],1)
+if ploti==1
+  % Plots and Cosmetics
+  plot(X,Y,'o')
+  % set(gca,'ytick',1:ld,'yticklabel',letters(1:ld,1))
+  set(gca,'ydir','rev')
+  fig2print(gcf,'portrait')
+  ylim(xpand(ylim))
+  % longticks(gca,2)
+  figdisp([],[],[],0)
+end
