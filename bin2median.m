8f37aa17662cc64d04913d0052e5129e174a3fe4
Frederik Simons <fjsimons@alum.mit.edu>
2020-08-20 00:35:04 -0400

    Summarizes data along non-overlapping one-dimensional bins

--- bin2median.m
+++ bin2median.m
@@ -1,23 +1,31 @@
-function [xi,ypi]=bin2median(x,y,M,perc)
-% [xi,ypi]=BIN2MEDIAN(x,y,M,perc)
+function [xi,ypi]=bin2median(x,y,multp,perc)
+% [xi,ypi]=BIN2MEDIAN(x,y,multp,perc)
 %
 % INPUT:
 %
 % x       The independent variable, not necessarily unique or equally spaced
 % y       The same-size dependent variable
-% M       The median interval multiplier over which the statistics will be quoted
+% multp   The median interval multiplier over which the statistics will be quoted
 % perc    The percentiles [default: 5 50 95]
 %
 % OUTPUT:
 %
-% xi      New independent variables
+% xi      New independent variables, midpoints of the requested intervals
 % ypi     A matrix with columns according to the requested percentages
 % 
 % EXAMPLE:
 % 
-% x=rand(100,1);
-% y=rand(100,1);
-% [xi,ypi]=bin2median(x,y,5,[5 50 95]);
+% N=10000;
+% x=rand(N,1);
+% x=[x ; x(randi(N,100,1))]';
+% y=randn(length(x(:)),1);
+% [xi,ypi]=bin2median(x,y,round(sqrt(N)),[5 50 95]);
+% plot(x,y,'b.')
+% hold on
+% plot(xi,ypi(:,2),'k','LineWidth',1)
+% plot(xi,ypi(:,1),'r')
+% plot(xi,ypi(:,3),'r')
+% hold off
 %
 % Last modified by fjsimons-at-alum.mit.edu, 08/19/2020
 
@@ -25,46 +33,51 @@ function [xi,ypi]=bin2median(x,y,M,perc)
 x=x(:);
 y=y(:);
 
-keyboard
-
-% Prepare for needing 
-[xu,IA,IX]=unique(x);
-
 % First we handle duplicate data points
+[xu,IA]=unique(x);
+
+if length(xu)~=length(x)
+  % Rejig a bit. In BENDERX I used ROW2STATS
+  [xu,IA]=unique(x+(-1)^randi(2)*rand(length(x),1)*100000*eps);
+  disp('Have had to rejig')
+end
+
+if length(xu)~=length(x)
+  error('Still not unique')
+end
+
+% Reassign data
+y=y(IA);
+x=xu;
 
 % Then we go MAG2MED, GPS2MEDIAN
 beg=min(x);
+
 % Figure out the median sampling intervals
-newdt=median(diff(xu));
-intv=M*newdt;
+newdt=median(diff(x));
+intv=multp*newdt;
 
 % How many times will this interval - potentially - be repeated?
 ntms=ceil([max(x)-beg]/intv);
 % Initialize the medians vector
-meds=nan(ntms,length(perc(:)));
+ypi=nan(ntms,length(perc(:)));
 
-% Snap every value to the nearest increment of newdt seconds, this is better for data drops 
+% Snap every value to the nearest increment of newdt 
 newt=round(x/newdt)*newdt;
-
-% Could consider reporting norm([x-newt]) to get a feel for the interpolation
 
 % Interpolate the data to the median sampling intervals
 yi=interp1(x,y,newt);
 
-% Rearrange them by sets of intv at a time, or nearly so, almost all
-multp=round(intv/newdt);
-% Report in samples if you must
-disp(sprintf('The number of samples taken together consecutively is %i',...
-	     multp))
 % This is roughly how many of those will find into the vector that you have
 multc=floor(length(yi)/multp);
 % Could have saved us the initialization. Compute the medians
-meds=nanmedian(reshape(yi(1:multp*multc),multp,multc),1);
+ypi=prctile(reshape(yi(1:multp*multc),multp,multc),perc)';
+
 % But... there's a couple you might have missed, so add their medians also
-meds=[meds nanmedian(yi(multp*multc+1:end))];
-toc
+ypi=[ypi ; prctile(yi(multp*multc+1:end),perc)];
+
 % From this you can learn at which time "meds" should be quoted
-tims=newt([round(multp/2):multp:multp*multc ...
+xi=newt([round(multp/2):multp:multp*multc ...
            multp*multc+round([length(yi)-multp*multc]/2)])';
 
 
