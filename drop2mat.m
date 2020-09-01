dacce4b9f972cd9306f9e4ff3e7d4cbd07030d51
Frederik J. Simons <fjsimons@gmail.com>
2020-08-31 23:43:27 -0400

    Reads, converts, and resaves Kestrel Drop data

--- drop2mat.m
+++ drop2mat.m
@@ -1,10 +1,10 @@
 function varargout=drop2mat(fname)
 % [t,d]=DROP2MAT(fname)
 %
-% Reads, and converts a CSV file from the Kestrel Drop instrument to a
-% MATLAB file including proper date-time variables. The format of the data
-% line is presumed to be (everything being given in metric SI units) as: 
-% 2020-08-16 17:16:00,"24,9","61,3","24,8","16,9"
+% Reads, and converts a CSV file from Kestrel Drop instruments to a MATLAB
+% file including proper date-time variables. The format of the data line is
+% presumed to be (everything being given in metric SI units) as: 2020-08-16
+% 17:16:00,"24,9","61,3","24,8","16,9"
 %
 % INPUT:
 %
@@ -23,14 +23,18 @@ function varargout=drop2mat(fname)
 %
 % drop2mat('demo1')
 %
-% Last modified by fjsimons-at-alum.mit.edu, 08/24/2020
+% TESTED ON:
+%
+% MATLAB Version: 9.4.0.813654 (R2018a)
+% 
+% Last modified by fjsimons-at-alum.mit.edu, 08/31/2020
 
 if isempty(strfind(fname,'demo'))
   % Prepare to save the CSV file as a MAT file
   [aa,bb,cc]=fileparts(fname);
   ename=sprintf('%s.mat',bb);
   
-  if exist(ename)~=2 
+  if exist(ename)~=2
     % Open the file
     fid=fopen(fname);
 
@@ -41,7 +45,8 @@ if isempty(strfind(fname,'demo'))
     end
 
     % Read the rest as the "data"
-    a=textscan(fid,'%q%q%q%q%q','Delimiter',',');
+    % DROP 3 FIRE has two more than DROP 2 HS, they'll be empty
+    a=textscan(fid,'%q%q%q%q%q%q%q','Delimiter',',');
 
     % Close the file
     fclose(fid);
@@ -62,17 +67,20 @@ if isempty(strfind(fname,'demo'))
 
     % Pick out the data variable names
     vnames=h{4}; vnames(abs(vnames)==32)='';
-    % You'll now know there are FOUR variables of interest
-    [~,v1,v2,v3,v4]=strread(vnames,'%s%s%s%s%s','delimiter',',');
+    % You'll now know there are FOUR to SIX variables of interest (any empties?)
+    [~,v1,v2,v3,v4,v5,v6]=strread(vnames,'%s%s%s%s%s%s%s','delimiter',',');
 
     % Pick out the unit name strings
     index=5;
     vnames=h{index}; vnames(abs(vnames)==32)='';
-    % You'll now know there are FOUR units of interest
-    [~,u1,u2,u3,u4]=strread(vnames,'%s%s%s%s%s','delimiter',',');
+    % You'll now know there are FOUR to SIX units of interest (two empties?)
+    [~,u1,u2,u3,u4,u5,u6]=strread(vnames,'%s%s%s%s%s%s%s','delimiter',',');
 
+    % For the next loop, it's a DROP 2 or a DROP 3
+    maxi=5+~isempty(u5)*2;
+    
     % Give the variables their proper place
-    for index=2:5
+    for index=2:maxi
       % Don't redo the time, you've got it already
       b=char(a{index});
       % Comma goes to decimal point
@@ -95,10 +103,8 @@ if isempty(strfind(fname,'demo'))
   end
 
 elseif strcmp(fname,'demo1')
-  % [t,d]=drop2mat('export_fjsimons_2020_8_20_19_13_54.csv');
-  % [t,d]=drop2mat('export_fjsimons_2020_8_21_17_36_49.csv');
-  [t,d]=drop2mat('export_fjsimons_2020_8_21_17_36_50.csv');
-  
+%  [t,d]=drop2mat('export_fjsimons_2020_8_21_17_36_50.csv');
+  [t,d]=drop2mat('export_fjsimons_2020_8_29_11_47_33.csv');
   if nargout==0
     % Make a picture
     col={'b' 'r' 'k'};
@@ -109,7 +115,7 @@ elseif strcmp(fname,'demo1')
     t.TimeZone=tz; 
     lb=plot(t,d.Temperature,col{1}); hold on
     index=0; maxrain=0; 
-    for jday=229:234
+    for jday=229:240
       index=index+1;
       yyaxis left
       [dd,h]=guyotweather(jday); dd.Timestamp.TimeZone=tz;
@@ -134,6 +140,7 @@ elseif strcmp(fname,'demo1')
     grid on; longticks(gca,2)
     yyaxis left 
     yl=ylabel(sprintf('Air temperature (%sC)',str2mat(176)));
+    % The 'AutoUpdate','off' pair only works on the latest versions
     leg=legend('Leabrook Lane','Guyot Hall','Location','NorthWest',...
 	       'AutoUpdate','off');
 
