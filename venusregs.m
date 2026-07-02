7eb5aeb6a5a49a63bba1edcf242fd6f152dc2cf6
Frederik J. Simons <fjsimons@gmail.com>
2026-07-02 17:26:03 -0400

    Loads Venusian topography and radar data regionalization info

--- venusregs.m
+++ venusregs.m
@@ -1,5 +1,5 @@
-function varargout=venusregs(id,ifplot,lolav,fax)
-% [lola,p]=VENUSREGS(id,ifplot,lolav,fax)
+function varargout=venusregs(id,ifplot,lolav,fax,fillco)
+% [lola,p,lolav]=VENUSREGS(id,ifplot,lolav,fax,fillco)
 %
 % Plots Venus regions data
 %
@@ -9,16 +9,24 @@ function varargout=venusregs(id,ifplot,lolav,fax)
 % ifplot   1 Makes simple two-dimensional lon/lat plot
 %          2 Makes three-dimensional hemisphere lon/lat plot
 %          3 Makes three-dimensional full-sphere lon/lat plot
+%          4 Make a two-dimensional filled color patch plot
 % lolav    The location of the viewing platform
 % fax      Axis scaling
+% fillco   The fill color
 %
 % OUTPUT:
 %
 % lola     Longitudes and latitudes of the region in question
+% p        The handle to the plot object(s), to be reviewed
+% lolav    Longitudes and latitudes of the viewing platform
 %
 % SEE ALSO:
 %
 % VENUSTATS, LORIS1
+%
+% EXAMPLE:
+%
+% for index=1:77; venusregs(index,1); hold on; end; axis([0 360 -90 90])
 %
 % Last modified by fjsimons-at-alum.mit.edu, 07/02/2026
 
@@ -26,6 +34,8 @@ defval('id',ceil(rand*77))
 defval('ifplot',round(rand))
 defval('p',NaN)
 defval('fax',1)
+defval('fillco',rand(1,3))
+defval('dlev',30)
 
 % This is a very small piece of LOADITMAKEIT within VENUSTATS... there is more
 load('/data1/fjsimons/IFILES/VENUS/DATA/plmData/plmVenus_D-5.mat',...
@@ -38,16 +48,32 @@ defval('lolav',eval(sprintf('V%4.4i_03.geo.center360',id)))
 
 switch ifplot
   case 1
-    twoplot(lola)
-  case 2
+    p=twoplot(kindeks(penlift([lola zeros(size(lola,1),1)],dlev),1:2));
+  case 4
+    lola=kindeks(penlift([lola zeros(size(lola,1),1)],dlev),1:2);
+    % If straddling the 0 line, it needs to be become two patches
+    rnan=find(isnan(lola(:,1)));
+    if ~isempty(rnan)
+        news=reshape([1  indeks([rnan(:)'-1 rnan(:)'+1],'1:end') size(lola,1)],2,[])';
+        for index=1:size(news,1)
+            renj=news(index,1):news(index,2);
+            p(index)=fill(lola(renj,1),lola(renj,2),fillco);
+            hold on
+        end
+        hold off
+    else
+        p=fill(lola(:,1),lola(:,2),fillco);
+    end
+  case {2,3}
     % Set view angles ahead of time as an explicit longitude and latitude
-    [xv,yv,zv]=sph2cart(lolav(:,1)*pi/180,lolav(:,2)*pi/180,1);
+    [xv,yv,zv]=sph2cart(lolav(1)*pi/180,lolav(2)*pi/180,1);
     % Now prepare to plot the actual data
     [xr,yr,zr]=sph2cart(lola(:,1)*pi/180,lola(:,2)*pi/180,1);
-    XYZ=[xr yr zr];
-    
-    % Plot only the visible semiglobe
-    yes=[xv yv zv]*XYZ'>0; XYZ=XYZ(yes,1:3);
+    if ifplot==2
+        % Plot only the visible semiglobe - look over the limb? - join with "equator"?
+        yes=[xv yv zv]*[xr yr zr]'>0;
+        xr=xr(yes); yr=yr(yes); zr=zr(yes);
+    end
     % This protection from jumps is straight from PLOTCONT
     defval('necessary',1)
     if necessary
@@ -55,21 +81,29 @@ switch ifplot
                (yr(2:end)-yr(1:end-1)).^2+...
                (zr(2:end)-zr(1:end-1)).^2);
         % Somehow with Venus boundaries the dlev needs to be pretty high!
-        dlev=30; pp=find(d>dlev*nanmedian(d));
+        pp=find(d>dlev*nanmedian(d));
         xr=insert(xr,NaN,pp+1); yr=insert(yr,NaN,pp+1); zr=insert(zr,NaN,pp+1);
     end
     % And then finally do it
     skl=0.99;
-    pc=plot3(xr(:)*skl,yr(:)*skl,zr(:)*skl,'k-');
-    hold on
+    % The next line could have been empty and then don't get to toggle hold
+    p=plot3(xr(:)*skl,yr(:)*skl,zr(:)*skl,'k-');
+    if ~isempty(p)
+        hold on
+    end
     % And now plot an entire equatorial circle also
     [xe,ye,ze]=sph2cart(linspace(0,2*pi,100),0,1);
     xyze=[rotz(-lolav(1)*pi/180)*roty(-[90-lolav(2)]*pi/180)*[xe ; ye ; repmat(ze,1,length(ye))]]';
     peq=plot3(xyze(:,1),xyze(:,2),xyze(:,3),'k');
-    hold off
+    if isempty(p)
+        hold on
+    end
     % Now set (and verify the syntax) of the view 
     view([xv,yv,zv]); [AZ,EL]=view;
     disp(sprintf('Azimuth: %i ; Elevation: %i',round(AZ),round(EL)))
+    pnp=plot3(xv,yv,zv,'MarkerF','k','MarkerE','k','Marker','o');
+    hold off
+    % Cosmetics
     axis equal; axis([-1 1 -1 1 -1 1]*fax);
     xl=xlabel('x'); yl=ylabel('y'); zl=zlabel('z');
     set(gca,'xtick',[-fax 0 fax],'ytick',[-fax 0 fax],'ztick',[-fax 0 fax])
@@ -81,5 +115,5 @@ switch ifplot
 end
 
 % Optional output
-varns={lola,p};
+varns={lola,p,lolav};
 varargout=varns(1:nargout);
