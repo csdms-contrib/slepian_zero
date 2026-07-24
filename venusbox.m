f3e2af33ebbf31348b42c4be603ec79296a399d2
Frederik Simons <fjsimons@alum.mit.edu>
2026-07-24 15:36:29 +0200

    Box plots for Venusian topography and radar data

--- venusbox.m
+++ venusbox.m
@@ -13,7 +13,7 @@ function varargout=venusbox(id,iftopo)
 % ph       Handles to the boxes
 % pm       Handles to the means
 % pd       Handles to the medians
-% ps       Handles to the standard deviations
+% psn       Handles to the standard deviations
 %
 % Last modified by fjsimons-at-alum.mit.edu, 07/24/2026
 
@@ -23,13 +23,20 @@ defval('iftopo',1)
 % If you've done this before, note you always know there are 77 regions
 if iftopo==1
     fname=fullfile(getenv('IFILES'),'VENUS/DATA/plmData/plmVenus_D-5_stats.mat');
+    % Should have saved that in there, but didn't yet reran VENUSTATS quickly
+    pc=   [-1.2697 -0.5850 -0.1817  0.3771 2.9456]*1e3;
+    percx=[ 2.5    25      50      75     97];
 else
     fname=fullfile(getenv('IFILES'),'VENUS/DATA/radarData/radVenus_D-5_stats.mat');
+    % Should have saved that in there, but didn't yet reran VENUSTATS quickly
+    pc=   [ 0    2.8284  3.4032  4.0180  5.6302]*1e4;
+    percx=[ 2.5 25      50      75      97];
 end
 
 % Just work from what was saved, even though not quite a whole box plot
 if exist(fname)
-    load(fname)
+    % Make sure you don't load all the actual global data, definitely not for radar
+    load(fname,'s')
 else
     error('Run VENUSTATS first!')
 end
@@ -38,18 +45,25 @@ end
 clf
 ah=gca;
 % Figure out some geometrics
-shifs=[77:-1:1]; tofs=0.3;
+cdown=[length(s.mean):-1:1];
+% Count down, box height, top and bottom
+shifs=cdown; tofs=0.3;
 strts=[shifs(:)+tofs shifs(:)-tofs];
 % Do the actual plotting
-ph=fillbox([s.p25(:) s.p75(:) strts],'w');
-pv=fillbox([s.pmean(:)-sqrt() s.pmean(:) strts],'w');
+
+% Horizontal bars, e.g. plot([1 2 ; 3 4 ; 5 6  ]',[1 1 ; 2 2 ; 3 3 ]');
+ps=plot([s.mean-2*sqrt(s.variance) ; s.mean+2*sqrt(s.variance)],[cdown ; cdown],'Color',grey);
 hold on
+ph=fillbox([s.p25(:) s.p75(:) strts],'w');
+% Vertical bars, e.g. plot([1 2 3 ; 1 2 3],[1 2 ; 2 3 ; 3 4]');
 pd=plot([s.median ; s.median],strts','k');
 pm=plot([s.mean   ; s.mean  ],strts','b');
+
 % Plot one more, the special one
+ps(end+1)=plot([s.mean(id)-2*sqrt(s.variance(id)) ; s.mean(id)+2*sqrt(s.variance(id))],[id ; id],'b');
 ph(end+1)=fillbox([s.p25(id) s.p75(id) strts(id,:)],'b');
-pd(end+1)=plot([s.median   ; s.median  ],strts','k');
-pm(end+1)=plot([s.mean(id) ; s.mean(id)],strst(id,:),'y');
+pd(end+1)=plot([s.median(id)   ; s.median(id)],strts(id,:),'r');
+pm(end+1)=plot([s.mean(id)     ; s.mean(id)  ],strts(id,:),'y');
 hold off
 
 if iftopo==1
@@ -62,8 +76,11 @@ ylabel('region number')
 % Cosmetix
 set(pm,'LineWidth',2)
 set(ah,'YTick',1:5:77)
+set(ah,'XTick',pc)
+set(ah,'XTickLabel',percx)
+set(ah,'XGrid','on')
 longticks(ah)
 
 % Optional output
-varns={ph,pm};
+varns={ph,pm,pd,ps};
 varargout=varns(1:nargout);
